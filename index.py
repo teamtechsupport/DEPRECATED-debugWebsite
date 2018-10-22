3849a349e557db7e7bbb438d867fd4191eebf7e7
Praveen Kathirvasan <larchpraveen22@outlook.com>
2018-10-22 16:55:06 +0100

    duh

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 import annealing_decryption
 import re
 import string
+import json
 
 key = string.ascii_uppercase
 ciphertype = "substitution"
@@ -11,10 +12,19 @@ ciphertype = "substitution"
 def my_form():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def my_form_post():
-	text = request.form['text']
-	userinput = text.upper()
-	regex = re.compile('[^A-Z]')
-	print(regex.sub('', userinput), "HE")
-	return (annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", ""))
+    selected = []
+    if request.form.get('substition'):
+        selected.append("substition")
+    if request.form.get('vigenere'):
+        selected.append("vigenere")
+    if request.form.get('columntrans'):
+        selected.append('columntrans')
+    print(json.dumps(selected))
+    text = request.form['text']
+    userinput = text.upper()
+    regex = re.compile('[^A-Z]')
+    print(regex.sub('', userinput), "HE")
+    decoded = annealing_decryption.anneal(regex.sub('', userinput), key, ciphertype, "swap", "")
+    return render_template("result.html", data = decoded)
